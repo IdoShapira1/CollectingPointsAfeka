307e8d248f2d3f436e8ee71cc2ff6fec23e38a1d
Yaron <yaron919@gmail.com>
2018-07-14 11:42:43 +0300

    change google map api key

--- app/src/main/java/com/final_project_afeka/collectingpointsfinal/PointsInsert.java
+++ app/src/main/java/com/final_project_afeka/collectingpointsfinal/PointsInsert.java
@@ -2,12 +2,37 @@ package com.final_project_afeka.collectingpointsfinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class PointsInsert extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class PointsInsert extends AppCompatActivity implements OnMapReadyCallback{
+
+    GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_points_insert);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        float zoom = (float) 15.0;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,zoom));
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
     }
 }
